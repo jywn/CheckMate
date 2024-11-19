8932f87c38c2e274b43505658ba9796f9baad611
jywn <jywon1128@gmail.com>
2024-11-19 19:38:40 +0900

    prompt updated

--- landing/utils/gpt_parser.py
+++ landing/utils/gpt_parser.py
@@ -8,14 +8,17 @@ Document = {document}
 
 |Start of task instructions|
 - Only follow the output format defined.
-- You are not allowed to output text between |Start of task instructions| and |End of output format instructions
+- You are not allowed to output text between |Start of task instructions| and |End of output format instructions|
 |End of task instruction|
 
 |Start of output format instructions|
-- Extract the following information from the document text: address or location, date
+- Extract the following information from the document text: address or location, date, people, title
+- location: location where you meet. if there is no location in input string, its null.
+- date: time when you meet.
+- people: people who you meet. if there is no people in input string, its null.
+- title: summarize the input sentence
 - Create a json like the form defined in |Start of json format instructions| and [End]
-- The start format is yyyymmddT0000Z format by picking the start date
-The end format is the same as the start date, but just make the time 230000Z. If the start is 20231004T000000Z, you can make the end 20231004T230000Z
+- The format is yyyymmdd0000 format by picking the date
 - If multiple date information is included, or if there is only one date information, make it json array from root element
 
 |End of output format instructions|
@@ -24,48 +27,28 @@ The end format is the same as the start date, but just make the time 230000Z. If
 [
   [
     "location": "파주 봉일천 중학교",
-    "start": "20231004T000000Z",
-    "end": "20231004T230000Z"
-  ],
-  [
-    "location": "파주 봉일천 중학교",
-    "start": "20231129T000000Z",
-    "end": "20231129T230000Z"
+    "date": "202310041400",
+    "people": "이민수",
+    "title": "이민수와 봉일천 중학교에서 14시",
   ]
 ]
 
 |End of json format instructions|
 """
 
-tmp = """
-입력 문장에서 다음과 같은 규칙을 따라서 JSON을 만들어줘.
-세 field는 time, people, place로 규칙은 다음과 같아.
-
-1. time은 YYYYMMDDhhmm 형식으로 반환되어야 해.
-4일 후를 입력 받으면, 지금으로부터 4일 후를 저장해.
-예를 들어, 1월 1일에 3일 후를 입력받으면, 1월 4일을 저장해.
-어떠한 날짜나 시간도 존재하지 않는다면 null을 저장해.
-
-2. people은 문자열 형식으로 사람이나 집단의 이름을 저장해.
-어떠한 사람이나 집단도 존재하지 않는다면 null을 저장해.  
-
-3. place는 문자열 형식으로 약속 장소를 저장해.
-어떠한 장소도 존재하지 않는다면 null을 저장해.
-
-4. python에서 사용할 것이니 홑따옴표 \'\'\' 대신 쌍따옴표 \"\"\"를 사용해서 둘러싸.
-"""
-
 def call_gpt_parser(input_string):
     client = OpenAI(
-        api_key="none"
+        api_key="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"
     )
 
     completion = client.chat.completions.create(
         # AI model
         model="gpt-4o",
         messages=[
-            {"role": "system", "content": tmp},
+            {"role": "system", "content": template},
             {"role": "user", "content": "지금은 " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + input_string},
         ]
     )
     return completion.choices[0].message.content.strip("```").strip("json")
+
+print(call_gpt_parser("내일 오후 3시에 김태균을 만날거야."))
